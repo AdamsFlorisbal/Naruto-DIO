74a8240e26f87e2cd46896a3987f3137b817d5f8
AdamsFlorisbal <adamsgawlinski@hotmail.com>
2022-06-25 22:47:32 -0300

    add animation carousel

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -1,4 +1,4 @@
-function handleMouseEnter(){
+function handleMouseEnter() {
     this.classList.add('n-card--hovered')
     document.body.id = `${this.id}-hovered`
 }
@@ -9,19 +9,32 @@ function handleMouseLeave() {
 
 }
 
-function addEventListenersToCards(){
+function addEventListenersToCards() {
     const cardElements = document.getElementsByClassName('n-card')
-    for(let i = 0; i < cardElements.length; i++){
+    for (let i = 0; i < cardElements.length; i++) {
         const card = cardElements[i]
         card.addEventListener('mouseenter', handleMouseEnter)
         card.addEventListener('mouseleave', handleMouseLeave)
 
     }
-    
+
 }
 
 document.addEventListener("DOMContentLoaded", addEventListenersToCards, false)
 
-function selectCarousrlItem(selectedItem){
-    console.log(selectedItem)
+function selectCarousrlItem(selectedButtonElement) {
+    const selectedItem = selectedButtonElement.id;
+    
+    const carousel = document.querySelector('.n-cards-carousel');
+    console.log(carousel)
+    const transform = carousel.style.transform;
+    const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+    const rotateYDeg = -120 * (Number(selectedItem) - 1);
+    const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+
+    carousel.style.transform = newTransform;
+
+    const activeButtonElement = document.querySelector('.n-controller__button--active');
+    activeButtonElement.classList.remove('n-controller__button--active');
+    selectedButtonElement.classList.add('n-controller__button--active');
 }
